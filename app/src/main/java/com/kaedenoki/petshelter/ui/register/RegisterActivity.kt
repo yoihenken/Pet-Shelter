ec6be1f46150aba1300ade340a2c030a404c616e
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-02-11 01:23:37 +0700

    add save item

--- app/src/main/java/com/kaedenoki/petshelter/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/kaedenoki/petshelter/ui/register/RegisterActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.kaedenoki.petshelter.R
 import com.kaedenoki.petshelter.databinding.ActivityRegisterBinding
+import com.kaedenoki.petshelter.model.DataSave
+import com.kaedenoki.petshelter.repository.local.SaveService
 import com.kaedenoki.petshelter.utils.Helpers.hideKeyboard
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private val model: RegisterViewModel by viewModels()
     private val TAG = "RegisterAct"
+    private val saveService = SaveService(application)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +28,8 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val itemAnimal = listOf("Cat", "Dog")
-        var animal: String
-        var type: String
+        var animal: String = ""
+        var type: String = ""
 
         binding.tfAnimal.apply {
             val adapter = ArrayAdapter(context, R.layout.item_dropdown, itemAnimal)
@@ -38,8 +44,23 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.apply {
+            btnSave.setOnClickListener {
+                if (!animal.equals("") && !type.equals("")) saveItem(animal, type)
+                else Toast.makeText(this@RegisterActivity, "Fill cant empty !", Toast.LENGTH_SHORT).show()
+            }
             btnCancel.setOnClickListener { finish() }
             btnBack.setOnClickListener { finish() }
+        }
+    }
+
+    private fun saveItem(animal: String, type : String) = with(binding){
+        lifecycleScope.launch {
+            saveService.addToSave(DataSave(
+                name = tfNama.editText?.text.toString(),
+                animal = animal,
+                type = type,
+                room = tfRoom.editText?.text.toString()
+            ))
         }
     }
 
