bdbb6b0a50d0b7c683f94613d3dffc01c75e859e
Bagus Bayu Sasongko <18104005@ittelkom-pwt.ac.id>
2021-02-10 17:48:59 +0700

    add layout home

--- app/src/main/java/com/kaedenoki/petshelter/ui/home/MainActivity.kt
+++ app/src/main/java/com/kaedenoki/petshelter/ui/home/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.kaedenoki.petshelter.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kaedenoki.petshelter.R
+import com.kaedenoki.petshelter.databinding.ActivityMainBinding
+import com.kaedenoki.petshelter.ui.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnRegister.setOnClickListener {
+                startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+            }
+        }
     }
 }
