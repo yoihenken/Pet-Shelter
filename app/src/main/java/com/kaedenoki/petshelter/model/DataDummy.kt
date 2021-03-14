614d3c09b5dffc78577cdad4ed6bb69585379a93
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-03-14 13:10:53 +0700

    update splashscreen

---
+++ app/src/main/java/com/kaedenoki/petshelter/model/DataDummy.kt
@@ -0,0 +1,17 @@
+package com.kaedenoki.petshelter.model
+
+class DataDummy {
+    val dummy = listOf(DataSave(name = "Catty", animal = "Cat", type = "Bengal", room = "Mawar"),
+        DataSave(name = "Chana", animal = "Cat", type = "Brazillian Shorthair", room = "Mawar"),
+        DataSave(name = "Neko", animal = "Cat", type = "Asian", room = "Mawar"),
+        DataSave(name = "Gerhan", animal = "Cat", type = "Asian", room = "Mawar"),
+        DataSave(name = "Garfield", animal = "Cat", type = "Korat", room = "Anggrek"),
+        DataSave(name = "Shana", animal = "Cat", type = "Burmese", room = "Anggresk"),
+        DataSave(name = "Ana", animal = "Cat", type = "Ana", room = "Mawar"),
+        DataSave(name = "Black", animal = "Dog", type = "Bernedoodle", room = "Rafles"),
+        DataSave(name = "White", animal = "Dog", type = "American Pugabull", room = "Rafles"),
+        DataSave(name = "Yoru", animal = "Dog", type = "American Bulldog", room = "Rafles"),
+        DataSave(name = "Charlie", animal = "Dog", type = "Berger Picard", room = "Rafles"),
+        DataSave(name = "Pendro", animal = "Dog", type = "American Pugabull", room = "Rafles")
+    )
+}
--- app/src/main/java/com/kaedenoki/petshelter/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/kaedenoki/petshelter/ui/splashscreen/SplashActivity.kt
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.kaedenoki.petshelter.R
+import com.kaedenoki.petshelter.model.DataDummy
 import com.kaedenoki.petshelter.ui.home.MainActivity
+import com.kaedenoki.petshelter.ui.register.RegisterViewModel
 
 class SplashActivity : AppCompatActivity() {
+
+    private val modelRegister = RegisterViewModel()
+    private val dataDummy = DataDummy()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        dataDummy.dummy.forEach {
+            modelRegister.saveData(this.application, it)
+        }
 
         Handler(mainLooper).postDelayed({
             finish()
